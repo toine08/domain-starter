ac59d79190bcc923b07e0e2e62d2abda97912a71
toine <toineriedo@protonmail.ch>
2022-02-23 21:38:19 +0100

    contractabi, update app.js and css style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from 'ethers';
+import contractABI from './utils/contractABI.json';
+
+import polygonLogo from './assets/polygonlogo.png';
+import ethLogo from './assets/ethlogo.png';
+import { networks } from './utils/networks';
+
+
 
 // Constants
 const TWITTER_HANDLE = '0xtoto8';
 const BUILDSPACE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const TWITTER_BUILDSPACE = `https://twitter.com/${BUILDSPACE}`;
+const CONTRACT_ADDRESS = '0x151ddC087a84144eF4f79E946aD980720fBcc62A';
+const tld = '.king';
 
 
 
@@ -14,6 +24,7 @@ const App = () => {
 	const [currentAccount, setCurrentAccount] = useState('');
 	const [domain, setDomain] = useState('');
 	const [record, setRecord] = useState(''); 
+	const [network, setNetwork] = useState('');
     
 	// Implement your connectWallet method here
 	const connectWallet = async () => {
@@ -55,7 +66,45 @@ const App = () => {
 		} else {
 			console.log('No authorized account found');
 		}
+
 	};
+
+	const mintDomain = async () =>{
+		if(!domain) { return }
+		if(domain.length < 3){
+			alert('domain must be at least 3 characters long');
+			return;
+		}
+		const price = domain.length === 3 ? '0.3' : domain.length === 4 ? '0.3' : '0.1';
+		console.log("Minting domain", domain, " with price: ", price);
+		try{
+			const { ethereum } = window;
+			if (ethereum){
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
+
+				console.log("going to pop wallet now to pay gas...");
+				let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+
+				const receipt = await tx.wait();
+
+				if(receipt.status ===1){
+					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+
+					tx =  await contract.setRecord(domain, record);
+					await tx.wait();
+
+					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+					setRecord('');
+					setDomain('');
+				}else{ alert('Transaction failed! Please try again!');}
+			}
+
+		}catch(error){
+			console.error(error);
+		}
+	}
 	
 	const renderNotConnectedContainer = () => {
 		return(
@@ -70,28 +119,42 @@ const App = () => {
 
 
 	const renderInputForm = () =>{
-		return(
-			<div className='form-container'>
+		return (
+			<div className="form-container">
 				<div className="first-row">
 					<input
-						type='text'
+						type="text"
 						value={domain}
 						placeholder='domain'
-						onChange={e => setDomain(e.target.value)} 
+						onChange={e => setDomain(e.target.value)}
 					/>
 					<p className='tld'> {tld} </p>
 				</div>
-				<input 
+
+				<input
 					type="text"
 					value={record}
 					placeholder='whats ur title ?'
 					onChange={e => setRecord(e.target.value)}
-
 				/>
 
+				<div className="button-container">
+					<button className='cta-button mint-button' onClick={mintDomain}>
+						Mint
+					</button>  
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>  
+				</div>
+
 			</div>
-		)
-	};
+		);
+	}
+  
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
 
   return (
 		<div className="App">
@@ -102,9 +165,14 @@ const App = () => {
               		<p className="title"> 👑King Name Service</p>
               		<p className="subtitle">Your Royalty DNS for matic!</p>
            		 </div>
+					<div className='right'>
+						<img alt="Network logo" className='logo' src={ network.includes("Polygon ") ? polygonLogo : ethLogo}/>
+						{currentAccount ? <p>Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)}</p> : <p>Not connected</p>}
+					</div>
 				</header>
 			</div>
 			{!currentAccount && renderNotConnectedContainer()}
+			{currentAccount && renderInputForm()}
         <div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
 					<a
